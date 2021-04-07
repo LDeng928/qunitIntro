5cd0383d73c9c5bb91c737218bdc57eb85ac90d1
li <lideng928@gmail.com>
2021-04-07 14:54:47 -0400

    add province select list

--- js/Hst.js
+++ js/Hst.js
@@ -1,24 +1,23 @@
-export default class Hst{
-    calculate(amount, province){
-        let nTax;
-        switch(province){
-            case 'ON':
-                nTax = amount * .13;
-                break;
-            case 'MB':
-                nTax = amount * .12;
-                break;
-            case 'AB':
-                nTax = amount * .05;
-                break;
-            case 'QC':
-                nTax = amount * .14975;
-                break;
-            default:
-                throw "unimplemented";
-
-        }
-        return nTax.toFixed(2);
+export default class Hst {
+  calculate(amount, province) {
+    let nTax;
+    switch (province) {
+      case "ON":
+        nTax = amount * 0.13;
+        break;
+      case "MB":
+      case "BC":
+        nTax = amount * 0.12;
+        break;
+      case "AB":
+        nTax = amount * 0.05;
+        break;
+      case "QC":
+        nTax = amount * 0.14975;
+        break;
+      default:
+        throw "unimplemented";
     }
-
+    return nTax.toFixed(2);
+  }
 }
--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 import Hst from "./Hst.js";
 import "https://code.jquery.com/jquery-1.12.1.min.js";
 
-$(document).ready(()=>{
-    $("#calculate").click((evt)=>{
-        evt.preventDefault();
-        const nAmount = $("#price").val();
-        const oHst = new Hst();
-        $("#amount").html(nAmount);
-        $("#hst").html(oHst.calculate(nAmount, "ON"));
-    })
+$(document).ready(() => {
+  $("#calculate").click((evt) => {
+    evt.preventDefault();
+    const nAmount = $("#price").val();
+    const sProvince = $("#province").val();
+    const oHst = new Hst();
+    $("#amount").html(nAmount);
+    $("#province").html(sProvince);
+    $("#hst").html(oHst.calculate(nAmount, sProvince));
+  });
 });
-
